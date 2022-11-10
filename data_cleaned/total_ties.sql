d31c335abc00cb974840b26c9d225ea1145eb9c3
Sulayman Yaffa <sulayaffa@gmail.com>
2022-11-09 19:37:47 -0800

    Update total_ties.sql

--- data_cleaned/total_ties.sql
+++ data_cleaned/total_ties.sql
@@ -1,3 +1,16 @@
+/*
+First, I wanted to create 2 CTEs 
+The first CTE is to get the tie record at home and the second CTE is to get the tie record away from home
+I decided to SELECT the HomeTeam and create a CASE statement to get the win, tie, loss record
+But for this query we are focusing on the tie record
+Then using that query as a subquery to COUNT the number of time a team has tied
+Create a CTE with the previous query and do the same when the team plays away from home
+Join both tables
+Since Man City tied twice away from home and didn't at home, we can't add a null value with an integer so we use the IFNULL function
+And Maan City is the only team missing so I used the COALESCE function
+Finally, we can save the query as view in BigQuery then join with the other views to create the final table
+*/
+
 WITH home_draw_record_CTE AS (
 SELECT HomeTeam, COUNT(Record) AS home_draw_record
 FROM (

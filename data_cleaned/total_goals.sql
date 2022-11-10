36a801519100517be05b1059aba0dd7ca5eb8331
Sulayman Yaffa <sulayaffa@gmail.com>
2022-11-09 19:14:22 -0800

    Update total_goals.sql

--- data_cleaned/total_goals.sql
+++ data_cleaned/total_goals.sql
@@ -1,3 +1,11 @@
+/* 
+Using CTEs to get the total number of goals scored.
+Using the SUM function to add the number of goals scored
+Using the GROUP BY clause since we are using an aggregate function SUM
+We can now join the 2 CTEs and add the number of goals scored at home and away
+Finally, we can save the query as view in BigQuery then join with the other views to create the final table
+*/
+
 WITH home_goals AS (
   SELECT HomeTeam, SUM(FTHG) AS goals_scored_at_home
   FROM `portfolio-360521.premier_league2019.season_2019`
